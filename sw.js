84c95021c3049b6546fe688831ade73de7bec85e
mdnayonsarker1-art <mdnayonsarker1@gmail.com>
2026-07-22 21:06:10 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,46 @@
-const cacheName = 'gmail-keeper-v3';
+const cacheName = 'gmail-keeper-v5';
 const staticAssets = [
   './',
-  './index.html',
-  './my-gmail.html',
-  './sale-gmail.html',
-  './manifest.json'
+  'index.html',
+  'my-gmail.html',
+  'sale-gmail.html',
+  'manifest.json'
 ];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(staticAssets);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
+// Activate Event: Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys.filter(key => key !== cacheName).map(key => caches.delete(key))
       );
     })
   );
-  return self.clients.claim();
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  const req = e.request;
+// Fetch Event
+self.addEventListener('fetch', event => {
+  const req = event.request;
   const url = new URL(req.url);
 
+  // Ignore Non-GET requests (e.g. Firebase Firestore writes)
   if (req.method !== 'GET') return;
 
   if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
+    event.respondWith(cacheFirst(req));
   } else {
-    e.respondWith(networkAndCache(req));
+    event.respondWith(networkAndCache(req));
   }
 });
 
@@ -46,13 +53,13 @@ async function cacheFirst(req) {
 async function networkAndCache(req) {
   const cache = await caches.open(cacheName);
   try {
-    const refresh = await fetch(req);
-    if (refresh.status === 200) {
-      await cache.put(req, refresh.clone());
+    const fresh = await fetch(req);
+    if (fresh.status === 200) {
+      cache.put(req, fresh.clone());
     }
-    return refresh;
-  } catch (e) {
+    return fresh;
+  } catch (error) {
     const cached = await cache.match(req);
-    return cached;
+    return cached || Response.error();
   }
 }
